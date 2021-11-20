ed1d0e51d67454d9ac6ef694768f0a15b7426eb0
Brandon Sansone <bsansone@users.noreply.github.com>
2021-11-19 20:18:25 -0700

    Bump version

--- src/__tests__/validators.test.ts
+++ src/__tests__/validators.test.ts
@@ -13,12 +13,6 @@ import {
   getClosestTimeByN,
   MAX_DEFAULT_STRING_LENGTH,
   MAX_DESCRIPTION_LENGTH,
-  isValidUrl,
-  getUserDisplayStatus,
-  classNames,
-  sanitizePrivateProperties,
-  cleanBadWords,
-  cleanObjectOfBadWords,
 } from '../index';
 
 const STATUSES: { [key: string]: string } = STUDENT_STATUS_OPTIONS.reduce(
@@ -104,8 +98,6 @@ const SHORT_PASSWORD = AUTH_USER.password.substring(0, MIN_PASSWORD_LENGTH - 1);
 const INVALID_EMAIL = SUPPORT_EMAIL.replace('@', '');
 const LONG_EVENT_DESCRIPTION = 'x'.repeat(MAX_DESCRIPTION_LENGTH + 1);
 const LONG_BASE_STRING = 'x'.repeat(MAX_DEFAULT_STRING_LENGTH + 1);
-const BAD_WORD = 'asshole';
-const CLEANED_WORD = '*******';
 
 const toBeValid = (result: ValidationResult): void => expect(result.error).toBeUndefined();
 const toBeInvalid = (result: ValidationResult): void => expect(result.error).not.toBeUndefined();
@@ -490,154 +482,5 @@ describe(CREATE_EVENT_FORM, () => {
 
   it('should be an invalid create event - location (isOnlineEvent: false) - empty string space', () => {
     toBeInvalid(validateCreateEvent({ ...FORM, isOnlineEvent: false, location: EMPTY_STRING_SPACE }));
-  });
-});
-
-describe('isValidUrl', () => {
-  it('should be a valid url - https://wwww.google.com', () => {
-    expect(isValidUrl('https://wwww.google.com')).toBe(true);
-  });
-
-  it('should be a valid url - https://google.com', () => {
-    expect(isValidUrl('https://google.com')).toBe(true);
-  });
-
-  it('should be a valid url - http://wwww.google.com', () => {
-    expect(isValidUrl('http://wwww.google.com')).toBe(true);
-  });
-
-  it('should be a valid url - http://google.com', () => {
-    expect(isValidUrl('http://google.com')).toBe(true);
-  });
-
-  it('should be an invalid url - ssh://google.com', () => {
-    expect(isValidUrl('ssh://google.com')).toBe(false);
-  });
-
-  it('should be an invalid url - www.google.com', () => {
-    expect(isValidUrl('www.google.com')).toBe(false);
-  });
-
-  it('should be an invalid url - google.com', () => {
-    expect(isValidUrl('google.com')).toBe(false);
-  });
-
-  it('should be an invalid url - google', () => {
-    expect(isValidUrl('google')).toBe(false);
-  });
-});
-
-describe('getUserDisplayStatus', () => {
-  it('should equal correct Alumni display status', () => {
-    expect(getUserDisplayStatus(STATUSES.ALUMNI)).toEqual('Alumni of ');
-  });
-
-  it('should equal correct Grad display status', () => {
-    expect(getUserDisplayStatus(STATUSES.GRAD)).toEqual('Graduate Student at ');
-  });
-
-  it('should equal correct Freshman display status', () => {
-    expect(getUserDisplayStatus(STATUSES.FRESHMAN)).toEqual('Freshman at ');
-  });
-
-  it('should equal correct Sophmore display status', () => {
-    expect(getUserDisplayStatus(STATUSES.SOPHMORE)).toEqual('Sophmore at ');
-  });
-
-  it('should equal correct Junior display status', () => {
-    expect(getUserDisplayStatus(STATUSES.JUNIOR)).toEqual('Junior at ');
-  });
-
-  it('should equal correct Senior display status', () => {
-    expect(getUserDisplayStatus(STATUSES.SENIOR)).toEqual('Senior at ');
-  });
-
-  it('should equal correct Other display status', () => {
-    expect(getUserDisplayStatus(STATUSES.OTHER)).toEqual('Other at ');
-  });
-});
-
-describe('getClosestTimeByN', () => {
-  it('should return correct time for 3, 8, 2', () => {
-    expect(getClosestTimeByN(3, 8, 2)).toEqual('3:10');
-  });
-
-  it('should return correct time for 4, 1, 2', () => {
-    expect(getClosestTimeByN(4, 1, 2)).toEqual('4:10');
-  });
-
-  it('should return correct time for 2, 34, 2', () => {
-    expect(getClosestTimeByN(2, 34, 2)).toEqual('2:40');
-  });
-
-  it('should return correct time for 9, 60, 2', () => {
-    expect(getClosestTimeByN(9, 60, 2)).toEqual('10:00');
-  });
-});
-
-describe('classNames', () => {
-  it('should return a string of classnames joined by spaces', () => {
-    expect(classNames(['a', 'b', '', ' ', 'c ', ' d ', ' e'])).toEqual('a b c d e');
-  });
-
-  it('should return a empty string', () => {
-    expect(classNames([])).toEqual('');
-  });
-});
-
-describe('sanitizePrivateProperties', () => {
-  it('should sanitize all private properties', () => {
-    const withPrivateProperties = {
-      _test: '_test',
-      test: 'test',
-      nested: {
-        _nested: 123,
-        a: { _b: '_b', x: null },
-        _d: [],
-      },
-    };
-    const withoutPrivateProperties = {
-      test: 'test',
-      nested: {
-        a: { x: null },
-      },
-    };
-
-    expect(sanitizePrivateProperties(withPrivateProperties)).toEqual(withoutPrivateProperties);
-  });
-});
-
-describe('cleanBadWords', () => {
-  it('should remove bad words', () => {
-    expect(cleanBadWords(BAD_WORD)).toEqual(CLEANED_WORD);
-  });
-});
-
-describe('cleanObjectOfBadWords', () => {
-  it('should remove all bad words', () => {
-    const withBadWords = {
-      a: BAD_WORD,
-      b: 'hello',
-      c: {
-        d: BAD_WORD,
-        e: {
-          f: BAD_WORD,
-          g: 'test',
-        },
-      },
-    };
-    const withoutBadWords = {
-      a: CLEANED_WORD,
-      b: 'hello',
-      c: {
-        d: CLEANED_WORD,
-        e: {
-          f: CLEANED_WORD,
-          g: 'test',
-        },
-      },
-    };
-
-    expect(cleanObjectOfBadWords(withBadWords)).toEqual(withoutBadWords);
   });
 });
